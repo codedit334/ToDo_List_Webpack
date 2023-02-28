29b23c7a03656e89324e42aa9d6c944e5f9a2bb9
codedit334 <oussama7elabdioui@gmail.com>
2023-02-28 19:12:10 +0100

    Polishing code

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,33 +1,32 @@
-import displayImages from './images.js';
+import displayImages from "./images.js";
+import Todo from "./todo.js";
+import sortArray from "./sort-array/dist/index.mjs";
 
-import Todo from './todo.js';
-import sortArray from './sort-array/dist/index.mjs';
-
-const dataInput = document.querySelector('.data_input');
-const todoList = document.querySelector('.todo_list');
+const dataInput = document.querySelector(".data_input");
+const todoList = document.querySelector(".todo_list");
 
 let todoItems = [];
 
 export default class UI {
   static getItems() {
-    todoItems = JSON.parse(localStorage.getItem('todoItems'));
+    todoItems = JSON.parse(localStorage.getItem("todoItems"));
     return todoItems || [];
   }
 
   static filterByID(ID) {
     todoItems = this.getItems();
 
-    const filterTodoItems = todoItems.filter((element) => +element.index !== +ID);
-    filterTodoItems.forEach((element) => {
-      if (element.index > ID) element.index -= 1;
-    });
-    localStorage.setItem('todoItems', JSON.stringify(filterTodoItems));
+    const filterTodoItems = todoItems.filter(
+      (element) => +element.index !== +ID
+    );
+
+    localStorage.setItem("todoItems", JSON.stringify(filterTodoItems));
     this.displayItems();
   }
 
   static isChecked(check) {
-    if (check === true) return 'checked';
-    return 'notChecked';
+    if (check === true) return "checked";
+    return "notChecked";
   }
 
   static check() {
@@ -36,7 +35,7 @@ export default class UI {
     todoItems.forEach((elem) => {
       if (elem.completed === true) {
         document.querySelector(
-          `[data-id="${elem.index}"] > .todo_check`,
+          `[data-id="${elem.index}"] > .todo_check`
         ).checked = true;
       }
     });
@@ -45,7 +44,7 @@ export default class UI {
   static displayItems() {
     todoItems = this.getItems();
     const sortedArray = sortArray(todoItems, {
-      by: 'index',
+      by: "index",
     });
     todoItems = sortedArray;
 
@@ -55,11 +54,11 @@ export default class UI {
       todoItems[i].index = count;
       count += 1;
     }
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
 
     // Render HTML
     if (todoItems) {
-      todoList.innerHTML = '';
+      todoList.innerHTML = "";
       todoItems.forEach((element) => {
         todoList.innerHTML += ` <li data-id="${element.index}" data-valid="${element.completed}"><input type="checkbox"  class="todo_check" />
                 <input type="text" value="${element.description}"class="todo_input" />
@@ -78,25 +77,25 @@ export default class UI {
       const todoItem = new Todo(dataInput.value, false, todoItems.length + 1);
 
       todoItems.push(todoItem);
-      localStorage.setItem('todoItems', JSON.stringify(todoItems));
+      localStorage.setItem("todoItems", JSON.stringify(todoItems));
     }
   }
 
   static deleteItem(e) {
-    const dataId = e.target.parentElement.getAttribute('data-id');
+    const dataId = e.target.parentElement.getAttribute("data-id");
     this.filterByID(dataId);
   }
 
   static changeItem(e, val) {
-    const dataId = e.target.parentElement.getAttribute('data-id');
+    const dataId = e.target.parentElement.getAttribute("data-id");
 
     todoItems = this.getItems();
 
     todoItems.forEach((elm) => {
       if (+elm.index === +dataId) elm.description = val;
     });
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
-    window.location.reload();
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    this.displayItems();
   }
 
   static clearCompleted() {
@@ -104,34 +103,28 @@ export default class UI {
 
     todoItems = todoItems.filter((elem) => elem.completed !== true);
 
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
     this.displayItems();
   }
 
-  static preValidate(valid, event) {
-    const dataId = event.target.parentElement.getAttribute('data-id');
+  static validateByID(valid, event) {
+    const dataId = event.target.parentElement.getAttribute("data-id");
 
-    event.target.parentElement.setAttribute('data-valid', valid);
+    event.target.parentElement.setAttribute("data-valid", valid);
     const todoItems = this.getItems();
     todoItems.find((elem) => {
-      let i;
-      if (+elem.index === +dataId) {
-        elem.completed = valid;
-        i = 1;
-      } else i = 0;
-      return i;
+      if (+elem.index === +dataId) return elem.completed = valid;
     });
 
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
   }
 
   static validate(event) {
-    const isValid = event.target.parentElement.getAttribute('data-valid');
+    const isValid = event.target.parentElement.getAttribute("data-valid");
 
-    todoItems = this.getItems();
-    if (isValid === 'false') {
-      this.preValidate(true, event);
-    } else this.preValidate(false, event);
+    if (isValid === "false") {
+      this.validateByID(true, event);
+    } else this.validateByID(false, event);
   }
 
   // end
