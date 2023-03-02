d28bd686c2a4e7811127e50c215b89238e091145
codedit334 <oussama7elabdioui@gmail.com>
2023-03-02 21:18:15 +0100

    Fix linter errors

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,13 +1,15 @@
+/* eslint no-use-before-define: ["error", { "classes": false }] */
+
 // import displayImages from './images.js';
-import Todo from "./todo.js";
+import Todo from './todo.js';
 
 // UI interaction
-const formInput = document.querySelector(".data_input");
+const formInput = document.querySelector('.data_input');
 
 // Delete item
 const deleteEvent = () => {
-  document.body.querySelectorAll(".trashImg").forEach((element) => {
-    element.addEventListener("click", (event) => {
+  document.body.querySelectorAll('.trashImg').forEach((element) => {
+    element.addEventListener('click', (event) => {
       UI.deleteItem(event);
     });
   });
@@ -15,8 +17,8 @@ const deleteEvent = () => {
 
 // Change item
 const changeEvent = () => {
-  document.querySelectorAll(".todo_input").forEach((element) => {
-    element.addEventListener("change", (elm) => {
+  document.querySelectorAll('.todo_input').forEach((element) => {
+    element.addEventListener('change', (elm) => {
       const newValue = elm.target.value;
       UI.changeItem(elm, newValue);
     });
@@ -24,18 +26,18 @@ const changeEvent = () => {
 };
 
 // Add item
-window.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
+window.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
     UI.storeItem();
-    formInput.value = "";
+    formInput.value = '';
   }
 });
 
 // Clear completed
 const clearCompletedEvent = () => {
   document
-    .querySelector(".clear_completed")
-    .addEventListener("click", (event) => {
+    .querySelector('.clear_completed')
+    .addEventListener('click', (event) => {
       event.preventDefault();
       UI.clearCompleted();
     });
@@ -43,8 +45,8 @@ const clearCompletedEvent = () => {
 
 // Make completed
 const validateEvent = () => {
-  document.querySelectorAll(".todo_check").forEach((elem) => {
-    elem.addEventListener("change", (event) => {
+  document.querySelectorAll('.todo_check').forEach((elem) => {
+    elem.addEventListener('change', (event) => {
       UI.validate(event);
     });
   });
@@ -52,7 +54,7 @@ const validateEvent = () => {
 
 // Clear All
 const clearAllEvent = () => {
-  document.querySelector("#refreshImg").addEventListener("click", () => {
+  document.querySelector('#refreshImg').addEventListener('click', () => {
     localStorage.clear();
     UI.displayItems();
   });
@@ -66,13 +68,13 @@ function coreFlow() {
   clearAllEvent();
 }
 
-const sortArray = require("sort-array/dist");
+const sortArray = require('sort-array/dist');
 
 let todoItems = [];
 
 export default class UI {
   static getItems() {
-    todoItems = JSON.parse(localStorage.getItem("todoItems"));
+    todoItems = JSON.parse(localStorage.getItem('todoItems'));
     return todoItems || [];
   }
 
@@ -80,10 +82,10 @@ export default class UI {
     todoItems = this.getItems();
 
     const filterTodoItems = todoItems.filter(
-      (element) => +element.index !== +ID
+      (element) => +element.index !== +ID,
     );
 
-    localStorage.setItem("todoItems", JSON.stringify(filterTodoItems));
+    localStorage.setItem('todoItems', JSON.stringify(filterTodoItems));
     this.displayItems();
     return todoItems;
   }
@@ -101,10 +103,10 @@ export default class UI {
   }
 
   static renderHTML(obj) {
-    const todoList = document.querySelector(".todo_list");
+    const todoList = document.querySelector('.todo_list');
 
     if (obj) {
-      todoList.innerHTML = "";
+      todoList.innerHTML = '';
       obj.forEach((element) => {
         todoList.innerHTML += ` <li data-id="${element.index}" data-valid="${element.completed}"><input type="checkbox"  class="todo_check" />
                 <input type="text" value="${element.description}"class="todo_input" />
@@ -117,7 +119,7 @@ export default class UI {
   static displayItems() {
     todoItems = this.getItems();
     const sortedArray = sortArray(todoItems, {
-      by: "index",
+      by: 'index',
     });
     todoItems = sortedArray;
 
@@ -127,7 +129,7 @@ export default class UI {
       todoItems[i].index = count;
       count += 1;
     }
-    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
 
     // Render HTML
     this.renderHTML(todoItems);
@@ -136,14 +138,14 @@ export default class UI {
   }
 
   static storeItem() {
-    const dataInput = document.querySelector(".data_input");
+    const dataInput = document.querySelector('.data_input');
 
     if (dataInput.value.length > 0) {
       todoItems = this.getItems();
 
       const todoItem = new Todo(dataInput.value, false, todoItems.length + 1);
       todoItems.push(todoItem);
-      localStorage.setItem("todoItems", JSON.stringify(todoItems));
+      localStorage.setItem('todoItems', JSON.stringify(todoItems));
 
       UI.displayItems();
       return todoItems;
@@ -152,19 +154,19 @@ export default class UI {
   }
 
   static deleteItem = (e) => {
-    const dataId = e.target.parentElement.getAttribute("data-id");
+    const dataId = e.target.parentElement.getAttribute('data-id');
     this.filterByID(dataId);
   };
 
   static changeItem(e, val) {
-    const dataId = e.target.parentElement.getAttribute("data-id");
+    const dataId = e.target.parentElement.getAttribute('data-id');
 
     todoItems = this.getItems();
 
     todoItems.forEach((elm) => {
       if (+elm.index === +dataId) elm.description = val;
     });
-    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
     this.displayItems();
   }
 
@@ -173,26 +175,26 @@ export default class UI {
 
     todoItems = todoItems.filter((elem) => elem.completed !== true);
 
-    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
     this.displayItems();
   }
 
   static validateByID(valid, event) {
-    const dataId = event.target.parentElement.getAttribute("data-id");
+    const dataId = event.target.parentElement.getAttribute('data-id');
 
-    event.target.parentElement.setAttribute("data-valid", valid);
+    event.target.parentElement.setAttribute('data-valid', valid);
     const todoItems = this.getItems();
 
     todoItems.forEach((elem) => {
       if (+elem.index === +dataId) elem.completed = valid;
     });
-    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
   }
 
   static validate(event) {
-    const isValid = event.target.parentElement.getAttribute("data-valid");
+    const isValid = event.target.parentElement.getAttribute('data-valid');
 
-    if (isValid === "false") {
+    if (isValid === 'false') {
       this.validateByID(true, event);
     } else this.validateByID(false, event);
   }
