04802f40f5aa0289b88af0efbaebe5e62b91e833
codedit334 <oussama7elabdioui@gmail.com>
2023-03-02 08:42:04 +0100

    Polish code

--- src/modules/images.js
+++ src/modules/images.js
@@ -1,5 +1,3 @@
-/* istanbul ignore file */
-
 // Images
 import {default as enter} from '../assets/enter.png';
 import { default as refresh} from '../assets/refresh.png';
@@ -7,7 +5,6 @@ import {default as trash} from '../assets/trash.svg';
 
 // const refreshImg = document.getElementById('refreshImg');
 // const enterImg = document.getElementById('enter');
-/* istanbul ignore next */
 const displayImages = () => {
   document.querySelector('#enter').src = enter;
   document.getElementById('refreshImg').src = refresh;
