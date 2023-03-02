27ba4c7be26498f3a88a78c42101f4e997c1fe4b
codedit334 <oussama7elabdioui@gmail.com>
2023-03-02 16:31:51 +0100

    Add clearAll test

---
+++ src/modules/clearCompleted.test.js
@@ -0,0 +1,39 @@
+import UI from "./UI.js";
+
+beforeAll(() => {
+  window.localStorage.clear();
+  simulateUl();
+});
+
+const simulateUl = () => {
+  const ul = document.createElement("ul");
+  ul.className = "todo_list";
+  document.querySelector("body").appendChild(ul);
+};
+
+it("Clear Completed", () => {
+  let todoItems = [
+    {
+      index: 1,
+      description: "we have to apply test",
+      completed: true,
+    },
+    {
+      index: 2,
+      description: "Content 2",
+      completed: true,
+    },
+    {
+      index: 3,
+      description: "Content 3",
+      completed: false,
+    },
+  ];
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+
+  UI.clearCompleted();
+
+  todoItems = UI.getItems();
+
+  expect(todoItems.length).toBe(1);
+});
